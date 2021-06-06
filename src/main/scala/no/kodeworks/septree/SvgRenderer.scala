2c47f4226ac7af4781356711218f41bf4ad212f8
eirirlar <eirirlar@gmail.com>
2021-06-06 23:49:24 +0200

    svg strokes

--- src/main/scala/no/kodeworks/septree/SvgRenderer.scala
+++ src/main/scala/no/kodeworks/septree/SvgRenderer.scala
@@ -4,6 +4,8 @@ import java.nio.file.{Files, Path}
 import scala.io.Source
 
 object SvgRenderer extends App {
+  val minBaseLineWidth = 6
+
   def renderToFile(sepTree: SepTree, transformY: Double => Double = identity) = {
     val paths = renderPaths(sepTree, transformY)
     val gg = Source.fromResource("graphpaper.svg").getLines.toVector.dropRight(2).appended(s"\n$paths\n</g>\n</svg>").mkString("\n")
@@ -12,21 +14,30 @@ object SvgRenderer extends App {
 
   def renderPaths(sepTree: SepTree, transformY: Double => Double = identity) = {
     val hex = sepTree.hex
+    val baseLineWidth = math.max(minBaseLineWidth, sepTree.depth)
+    var curLevel = 0
 
-    val hexPaths = hex.toList(sepTree.depth).map { sh =>
+    val hexPaths = hex.toList(sepTree.depth).flatMap { sh =>
+      val maybeStroke = if (curLevel != sh.level) {
+        val stroke = baseLineWidth - curLevel
+        val maybeEndStroke = if (curLevel != 0) List("</g>") else Nil
+        curLevel = sh.level
+        maybeEndStroke ++ List(s"""<g stroke-width="$stroke">""")
+      } else Nil
       val css = sh.corners.toList.map(p =>
         s"${p.x},${transformY(p.y)}"
       )
-      s"""<path fill="none" stroke="rgb(128,128,128)" d="M ${css.head} L ${css.tail.mkString(" ")} Z" />"""
-    }
+      maybeStroke ++
+        List(s"""<path d="M ${css.head} L ${css.tail.mkString(" ")} Z" />""")
+    } ++ List("</g>")
     val x0 = sepTree.space.lowerLeft.x
     val y0 = sepTree.space.lowerLeft.y
     val x1 = sepTree.space.upperRight.x
     val y1 = sepTree.space.upperRight.y
-    val rect = s"""<path fill="none" stroke="rgb(128,128,128)" d="M $x0,$y1 L $x1,$y1 $x1,$y0 $x0,$y0 Z" />"""
+    val rect = s"""<path stroke-width="11" d="M $x0,$y1 L $x1,$y1 $x1,$y0 $x0,$y0 Z" />"""
     (rect :: hexPaths).mkString("\n")
   }
 
-  val sepTree = SepTree(Space(Point(5750d, 10100d), Point(15250, 19600d)), 7)
+  val sepTree = SepTree(Space(Point(5750d, 10100d), Point(15250, 19600d)), 5)
   renderToFile(sepTree, 29702d -)
 }
